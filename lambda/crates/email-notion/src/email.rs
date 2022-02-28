2aeead8d2f73e592a6bdd06fdde0c72c3242482c
Drew Kilb <drew.kilb@duke.edu>
2022-02-28 11:44:51 -0500

    feat: Traverse multipart messages to pull out bits we care about
    This commit implements a recursive function to traverse all subparts
    of messages, and grab plain text, images, and files.

--- lambda/crates/email-notion/src/email.rs
+++ lambda/crates/email-notion/src/email.rs
@@ -11,6 +11,7 @@ pub struct ParsedEmail {
 
 pub struct File {
     pub data: Vec<u8>,
+    pub id: Option<String>,
     pub name: String,
 }
 
@@ -25,7 +26,7 @@ pub fn parse_email(data: &[u8]) -> Result<ParsedEmail, MailParseError> {
     }
     let subject = parsed.headers.get_first_value("Subject").unwrap();
     let message_id = parsed.headers.get_first_value("Message-ID");
-    let result = ParsedEmail {
+    let mut result = ParsedEmail {
         body: String::from(""),
         files: vec![],
         from: from_addr,
@@ -33,5 +34,43 @@ pub fn parse_email(data: &[u8]) -> Result<ParsedEmail, MailParseError> {
         message_id,
         subject,
     };
+    process_subparts(&parsed.subparts, &mut result)?;
     Ok(result)
+}
+
+fn process_subparts(
+    subparts: &[ParsedMail],
+    result: &mut ParsedEmail,
+) -> Result<(), MailParseError> {
+    for part in subparts {
+        // Capturing any text part so it's not saved as a file
+        if part.ctype.mimetype.starts_with("text") {
+            // We're only using plain text for Notion task
+            if part.ctype.mimetype == "text/plain" {
+                result.body = part.get_body()?;
+            }
+        } else if part.ctype.mimetype.starts_with("image") {
+            let name = &part.ctype.params["name"];
+            let id = part.headers.get_first_value("Content-ID");
+            result.images.push(File {
+                id,
+                name: name.to_string(),
+                data: part.get_body_raw().unwrap(),
+            })
+        } else if !part.ctype.mimetype.starts_with("multipart") {
+            // I'm assuming anything not text or a multipart type is a file
+            // I need to save.
+            let name = &part.ctype.params["name"];
+            result.files.push(File {
+                data: part.get_body_raw().unwrap(),
+                id: None,
+                name: name.to_string(),
+            })
+        } else {
+            // This should only be parts with a content type of multipart.
+            // Multipart content should have subparts.
+            process_subparts(&part.subparts, result)?;
+        }
+    }
+    Ok(())
 }
